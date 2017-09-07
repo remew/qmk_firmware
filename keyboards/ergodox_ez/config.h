0fcde3abcafc2f346359df12383bdcfa15a4141b
remew <foolishremew@gmail.com>
2017-09-07 17:46:43 +0900

    change time to max mouse speed

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -35,7 +35,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define MOUSEKEY_INTERVAL       20
 #define MOUSEKEY_DELAY          0
-#define MOUSEKEY_TIME_TO_MAX    30
+#define MOUSEKEY_TIME_TO_MAX    10
 #define MOUSEKEY_MAX_SPEED      3
 #define MOUSEKEY_WHEEL_DELAY 0
 
