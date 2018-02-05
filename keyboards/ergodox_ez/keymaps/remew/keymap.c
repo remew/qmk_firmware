21fb290ebdcb71aa6cf983767c4b5d4d3db2c325
remew <foolishremew@gmail.com>
2018-02-05 09:26:18 +0900

    upate keymap

--- keyboards/ergodox_ez/keymaps/remew/keymap.c
+++ keyboards/ergodox_ez/keymaps/remew/keymap.c
@@ -37,8 +37,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |  ]   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      | LGui |MHEN/LAlt|                                 |HENK/RAlt| RGui |      |      |      |
- *   `-------------------------------------'                                 `-------------------------------------'
+ *   |      |      |      | LGui | LAlt |                                | RAlt | RGui |      |      |      |
+ *   `----------------------------------'                                `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      | Esc  |
  *                               ,--------|------|------|       |------+------+--------.
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, _______,
         KC_LCTRL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
          KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, _______,
-         _______, _______, _______, KC_LGUI, MT(KC_RALT, KC_MHEN),
+         _______, _______, _______, KC_LGUI, KC_LALT,
                                                       _______, _______,
                                                                _______,
                                      LT(MDIA,KC_SPC), KC_BSPC,  KC_DEL,
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LBRC,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSLS,
                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
         KC_RBRC,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-                          MT(KC_RALT, KC_HENK), KC_RGUI, _______, _______, _______,
+                          KC_RALT, KC_RGUI, _______, _______, _______,
         _______,  KC_ESC,
         _______,
          KC_INS,  KC_TAB, KC_ENT
